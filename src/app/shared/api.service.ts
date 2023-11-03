f0b259056065bfcbb56934a83a3cdc5555e3bea0
Pranto Bhoumik <143868960+PrantoBhoumik@users.noreply.github.com>
2023-11-03 22:09:58 +0530

    Update api.service.ts

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import student from '.';
 import {HttpClient} from '@angular/common/http';
 import{map}from 'rxjs/operators'
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+ studentList:any[]=student;
   constructor(private http: HttpClient) { }
   postStudent(data:any){
     return this.http.post<any>("http://localhost:3000/studentDetails",data)
@@ -14,9 +16,10 @@ export class ApiService {
     }))
   }
   getStudent(){
-    return this.http.get<any>("http://localhost:3000/studentDetails")
-    .pipe(map((res:any)=>{
-      return res;
+    // return this.http.get<any>("http://localhost:3000/studentDetails")
+   // .pipe(map((res:any)=>{
+     //return res;
+    return 
     }))
   }
   updateStudent(data:any,id:number){
